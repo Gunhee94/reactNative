c771630131d0bfbdfb8e58a5c763bc7b25e76384
gunhee94 <erjsgml94@naver.com>
2023-01-09 16:42:42 +0900

    스타일 레이아웃 만들기 진행중

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+ 
 export default function App() {
+
     return (
-        <View style={styles.container}>
-            <Text>Hi</Text>
+        <View style={styles.container} >
             <StatusBar style="auto" />
+            <View style={styles.bar}>
+                <Text style={styles.title}>귀여운거</Text>
+                <FontAwesome5 style={styles.hambuger} name="bars" size={30} color="black" />
+            </View>
+            
+            <View style={styles.body}>
+                <Text>dddd</Text>
+            </View>
+            {/* <Entypo name="baidu" size={100} color="white" /> */}
         </View>
     );
 }
@@ -13,8 +24,25 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        marginTop: 50,
+        flexDirection: "column",
         backgroundColor: "#c86e37",
-        alignItems: "center",
-        justifyContent: "center",
+    },
+    bar: {
+        flex: 0.5,
+        backgroundColor: "white",
+        flexDirection: "row",
+        padding: 20,
+    },
+    title: {
+        fontSize: 30,
+        flex: 1,
+    },
+    hambuger: {
+        felx: 1,
+    },
+    body: {
+        flex: 9.5,
+        backgroundColor: "white",
     },
 });
